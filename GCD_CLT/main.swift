701cc5f25a9a3acf7e6bc29e8decc4ed90d7caa7
Игорь Михайлович Ракитянский <i697@ya.ru>
2016-08-15 18:15:09 +0600

    Group

--- GCD_CLT/main.swift
+++ GCD_CLT/main.swift
@@ -10,32 +10,52 @@ import Foundation
 
 print("Hello, World!")
 
-class A {
-    var someVal = 0
+let firstGroup = dispatch_group_create()
 
-    let concurrentQueue = dispatch_queue_create("queue_for_property", DISPATCH_QUEUE_CONCURRENT)
-
-    @objc func increaseValueBy1000() {
-        dispatch_barrier_async(concurrentQueue) {
-            for _ in 0..<1000 {
-                let v = self.someVal + 1
-                print(v)
-                self.someVal = v
-            }
+dispatch_group_async(firstGroup, dispatch_get_global_queue(QOS_CLASS_DEFAULT, 0)) { 
+    for i in 0...1000 {
+        if i == 1000 {
+            print(i)
         }
     }
 }
 
-let instance = A()
-
-dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0)) {
-    instance.increaseValueBy1000()
+dispatch_group_async(firstGroup, dispatch_get_global_queue(QOS_CLASS_DEFAULT, 0)) {
+    for i in 0...2000 {
+        if i == 2000 {
+            print(i)
+        }
+    }
 }
 
-NSThread.detachNewThreadSelector(#selector(A.increaseValueBy1000), toTarget: instance, withObject: nil)
+dispatch_group_notify(firstGroup, dispatch_get_main_queue()) { 
+    print("first group finished old tasks")
+}
+
+let secondGroup = dispatch_group_create()
+
+dispatch_group_enter(secondGroup)
+for i in 0...3000 {
+    if i == 3000 {
+        print(i)
+    }
+}
+dispatch_group_leave(secondGroup)
+
+dispatch_group_enter(secondGroup)
+for i in 0...4000 {
+    if i == 4000 {
+        print(i)
+    }
+}
+dispatch_group_leave(secondGroup)
 
 
-instance.increaseValueBy1000()
+dispatch_group_notify(secondGroup, dispatch_get_main_queue()) {
+    print("second group finished old tasks")
+}
+dispatch_group_wait(secondGroup, DISPATCH_TIME_FOREVER)
+print("there are no tasks in second group")
 
 
 
