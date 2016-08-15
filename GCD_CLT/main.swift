d71b3cc400627ce5cdf9e0ccc08f9bee5a982d2d
Игорь Михайлович Ракитянский <i697@ya.ru>
2016-08-15 17:09:01 +0600

    Create

---
+++ GCD_CLT/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  GCD_CLT
+//
+//  Created by Игорь Михайлович Ракитянский on 15.08.16.
+//  Copyright © 2016 RIM. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+
+
+
+
+
+
+dispatch_main()
+
